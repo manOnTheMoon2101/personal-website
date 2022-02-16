1200d449a1d2e038818cb03657212cf093fa1067
manOnTheMoon2101 <clevejohnclayton.2101@gmail.com>
2022-02-16 10:11:52 +0200

    personal-website(nextJS)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: true,}
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: true,
 }
